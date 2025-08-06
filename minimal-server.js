995d60c73e9cfb4c87836a1749f36308d9a0da41
stephanyes <stephanoiucciolino@gmail.com>
2025-08-06 16:23:50 -0300

    chore: deleted testing files

--- minimal-server.js
+++
@@ -1,1 +0,0 @@
-require('http').createServer((req,res) => res.end('OK')).listen(3000)
--- src/pino2/index.ts
+++
@@ -1,6 +0,0 @@
-// export * from './types';
-// export * from './destinations';
-// export * from './logger';
-// export * from './simple-logger';
-// export { loggerApp, loggerAppRolling } from './factory';
-export * from './simple-logger';
